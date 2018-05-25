1baef03e216e215a537615ae95117e770a2db94d
ross-albertson <magnusthegood@yahoo.com>
2018-05-25 11:59:59 -0500

    First Push

---
+++ README.md
@@ -0,0 +1,16 @@
+# flutter_test2
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
+
+You can install MongoDB from [MongoDB](https://www.mongodb.com).
+
+Replace the value of uriString with your public IP address.
+
+You will need to run this code with Intellij IDEA. Before running, go to
+
+File > Settings > Languages & Frameworks > Flutter > Disable Dart 2
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:mongo_dart/mongo_dart.dart' as mongo;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new Names(pageName: "Facebook Code Test",),
+    );
+  }
+}
+
+class Names extends StatefulWidget {
+
+  final String pageName;
+
+
+  const Names({Key key, this.pageName}) : super(key: key);
+
+  @override
+  _NamesState createState() => new _NamesState();
+}
+
+class _NamesState extends State<Names> {
+  List<Name> _names = [];
+  List<Widget> _namesTiles = [];
+  String uriString = "mongodb://[2601:247:c301:7d94:7141:9ff9:e2e5:7d09]:27017/rpc1";
+  String collectionName = "names";
+
+  @override
+  void initState() {
+    super.initState();
+    _getNames();
+  }
+
+  _getNames() async {
+    mongo.Db db = new mongo.Db(uriString);
+    var collection = db.collection(collectionName);
+    await db.open();
+    await collection.find().forEach((map) {
+      Name name = new Name()
+          ..firstName = map['firstName']
+          ..lastName = map['lastName'];
+      _names.add(name);
+    });
+    setState(() {
+      for (var found in _names) {
+        Text text = new Text("$found");
+        _namesTiles.add(text);
+      }
+    });
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(title: new Text(widget.pageName),),
+      body: new ListView.builder(itemBuilder: (context, index) => _namesTiles[index],
+      itemCount: _namesTiles.length,),
+    );
+  }
+}
+
+
+
+class Name {
+  String firstName;
+  String lastName;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "$firstName $lastName";
+  }
+
+
+}
+
+
+
+
